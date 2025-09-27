b27159385e691df9a7ba1bf7df77621d8e16401e
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-09-26 20:29:55 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pokemonappg13/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
